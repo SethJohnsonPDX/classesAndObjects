3e9cf46a4c57676142e008e4a0c7c3d9624e9d26
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-05-01 17:50:29 -0700

    commit class "inheritance" updates

--- classesAndObjects/Program.cs
+++ classesAndObjects/Program.cs
@@ -10,23 +10,29 @@ namespace classesAndObjects
     {
         static void Main(string[] args)
         {
-            Deck deck = new Deck();
-            deck.Shuffle(3);
-
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " Of " + card.Suit);
-
-            }
-            Console.WriteLine(deck.Cards.Count);
+            TwentyOneGame game = new TwentyOneGame() { Dealer = "Seth", Name = "Twenty-One" };
+            game.Players = new List<string>() { "Jesse", "Bill", "Joe" };
+            game.ListPlayers();
+            game.Play();
             Console.ReadLine();
+
+            //    Deck deck = new Deck();
+            //    deck.Shuffle(3);
+
+            //    foreach (Card card in deck.Cards)
+            //    {
+            //        Console.WriteLine(card.Face + " Of " + card.Suit);
+
+            //    }
+            //    Console.WriteLine(deck.Cards.Count);
+            //    Console.ReadLine();
         }
 
-        //public static Deck Shuffle(Deck deck, int times)
+        //public static deck shuffle(deck deck, int times)
         //{
         //    for (int i = 0; i < times; i++)
         //    {
-        //        deck = Shuffle.deck();
+        //        deck = shuffle.deck();
         //    }
 
         //    return deck;
