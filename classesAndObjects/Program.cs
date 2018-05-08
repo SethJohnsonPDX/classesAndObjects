c0f4eefd45ec015970661c59e20a61713e741cb7
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-05-08 00:08:03 -0700

    commit page 55 enums

--- classesAndObjects/Program.cs
+++ classesAndObjects/Program.cs
@@ -10,21 +10,32 @@ namespace classesAndObjects
     {
         static void Main(string[] args)
         {
-            TwentyOneGame game = new TwentyOneGame() { Dealer = "Seth", Name = "Twenty-One" };
-            game.Players = new List<string>() { "Jesse", "Bill", "Joe" };
-            game.ListPlayers();
+            //Card card = new Card();
+            //card.Suit = Suit.Clubs;
+            //int underlyingValue = Convert.ToInt32(Suit.Diamonds);
+
+            // OPERATOR OVERLOADING //
+            //Game game = new TwentyOneGame();
+            //game.Players = new List<Player>();
+            //Player player = new Player();
+            //player.Name = "Jesse";
+            //game = game + player;
+
+            //TwentyOneGame game = new TwentyOneGame() { Dealer = "Seth", Name = "Twenty-One" };
+            //game.Players = new List<string>() { "Jesse", "Bill", "Joe" };
+            //game.ListPlayers();
+            //Console.ReadLine();
+
+            Deck deck = new Deck();
+            deck.Shuffle(3);
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " Of " + card.Suit);
+
+            }
+            Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
-
-            //    Deck deck = new Deck();
-            //    deck.Shuffle(3);
-
-            //    foreach (Card card in deck.Cards)
-            //    {
-            //        Console.WriteLine(card.Face + " Of " + card.Suit);
-
-            //    }
-            //    Console.WriteLine(deck.Cards.Count);
-            //    Console.ReadLine();
         }
 
         //public static deck shuffle(deck deck, int times)
